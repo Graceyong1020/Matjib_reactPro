a1e1d3c1ceb791d93563aeec18656b1d8defdfbd
GRACE\Baby <graceyong1020@gmail.com>
2024-12-21 04:49:02 -0800

    Install React Query

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import { RouterProvider } from "react-router-dom";
 import root from "./router/root";
 import "./App.css";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+const queryClient = new QueryClient();
 
 function App() {
-  return <RouterProvider router={root} />;
+  return (
+    <QueryClientProvider client={queryClient}>
+      {" "}
+      {/* react 동작할때 react query client도 초기화 */}
+      <RouterProvider router={root} />
+      <ReactQueryDevtools initialIsOpen={true} /> {/* 디버깅 도구 */}
+    </QueryClientProvider>
+  );
 }
 
 export default App;
